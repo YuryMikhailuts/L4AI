9ecf7fb9765363f5b9ec8f1a8b86f74b815d19d6
Михайлуц Юрий Вячеславович <aracks@yandex.ru>
2020-06-20 22:14:15 +0300

    Исправлены CMakeLists файлы так, что бы в получившемся eclipse проекте нормально работала поддержка C++17.

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+PROJECT_DIR=$PWD
+cd ..
 mkdir -p build
 cd build
-cmake "../" -G  "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ECLIPSE_VERSION=4.11
+cmake "$PROJECT_DIR" -G  "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug 
