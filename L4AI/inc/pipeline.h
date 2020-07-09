3152cf3c4493e55fcf3c1c0724aff061b194285e
Михайлуц Юрий Вячеславович <aracks@yandex.ru>
2020-07-09 07:26:57 +0300

    Добавлен класс линейного конвейера алгоритмов. Пока что он пустой.

---
+++ L4AI/inc/pipeline.h
@@ -0,0 +1,27 @@
+/*
+ * pipeline.h
+ *
+ *  Создано российским программистом на территории Российской Федерации.
+ *  (Created by a Russian programmer on the Russian Federation.)
+ *
+ *  Дата создания (Created): 9 июл. 2020 г.
+ *  Разработчик (Developer): Михайлуц Юрий Вычеславович (aracks@yandex.ru)
+ */
+
+#ifndef L4AI_INC_PIPELINE_H_
+#define L4AI_INC_PIPELINE_H_
+#include <pipe.h>
+
+namespace l4ai::algs {
+
+	class L4AI_EXPORT PipeLine : public Pipe {
+	private:
+		Algorithm* layers;
+	public:
+		PipeLine();
+		PipeLine(size_t layer_count, Algorithm* const layers = nullptr);
+	};
+
+}
+
+#endif /* L4AI_INC_PIPELINE_H_ */
---
+++ L4AI/src/pipeline.cpp
@@ -0,0 +1,13 @@
+/*
+ * pipeline.cpp
+ *
+ *  Создано российским программистом на территории Российской Федерации.
+ *  (Created by a Russian programmer on the Russian Federation.)
+ *
+ *  Дата создания (Created): 9 июл. 2020 г.
+ *  Разработчик (Developer): Михайлуц Юрий Вычеславович (aracks@yandex.ru)
+ */
+
+
+
+
